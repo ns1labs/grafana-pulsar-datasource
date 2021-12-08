587d18e0954a351a75276851873ffa0f928bab9f
Antonio Varela <antonio.varela@icon.mx>
2021-12-08 11:52:27 -0700

    Added basic health check.

--- pkg/main.go
+++ pkg/main.go
@@ -14,12 +14,12 @@ func main() {
 	// to exit by itself using os.Exit. Manage automatically manages life cycle
 	// of datasource instances. It accepts datasource instance factory as first
 	// argument. This factory will be automatically called on incoming request
-	// from Grafana to create different instances of SampleDatasource (per datasource
+	// from Grafana to create different instances of PulsarDatasource (per datasource
 	// ID). When datasource configuration changed Dispose method will be called and
-	// new datasource instance created using NewSampleDatasource factory.
+	// new datasource instance created using NewPulsarDatasource factory.
 	err := datasource.Manage(
 		"ns1labs-pulsar-datasource",
-		plugin.NewSampleDatasource,
+		plugin.NewPulsarDatasource,
 		datasource.ManageOpts{},
 	)
 	if err != nil {
