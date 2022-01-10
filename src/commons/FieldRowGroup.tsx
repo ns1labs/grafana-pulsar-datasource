49fdcc9937b6d5676c244bfffd3789ee52b6c0a2
Antonio Varela <antonio.varela@icon.mx>
2022-01-10 11:12:27 -0700

    Added licence header to source code files.

--- src/commons/FieldRowGroup.tsx
+++ src/commons/FieldRowGroup.tsx
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import React, { FC } from 'react';
 import { GrafanaTheme } from '@grafana/data';
 import { useStyles } from '@grafana/ui';
